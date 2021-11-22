444fd1d63881bb1d81a495459c394d67b2958654
mike whitfield <mwhitfieldwork@gmail.com>
2021-11-21 21:48:10 -0600

    Added post to login service, added methods in the component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import {LoginService} from './login.service'
+import {User} from '../shared/models/user'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private loginService:LoginService,  private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+
+  get form() { return this.loginForm.controls; }
+
+  LoginCheck():void {
+
+    let formData:User = {
+      PKID:0,
+      UserName:this.form.username.value,
+      Password:this.form.password.value
+    }
+
+    console.log(formData);
+
+    this.loginService.LoginFromAPI(formData)
+    .subscribe(
+      (data) => console.log(data),
+      (err:any) => console.log(err)
+    )
   }
 
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {catchError, tap, map } from 'rxjs/operators';
+import {User} from '../shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  url: string = 'http://localhost:54133/Login';
 
-  constructor() { }
+  nwDataChanged:BehaviorSubject<any>;
+  
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  constructor(private _http:HttpClient) {
+    this.nwDataChanged = new BehaviorSubject([]);
+   }
+
+   LoginFromAPI(user: User): Observable<any> {
+
+    return this._http.post<User>(this.url, JSON.stringify(user), this.httpOptions)
+  
+  }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error || 'Server error');
+  }
+
 }
