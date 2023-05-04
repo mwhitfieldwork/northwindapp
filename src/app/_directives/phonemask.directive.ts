9f42017227ba2256ec493cae726b433604935bd9
mike whitfield <whitfieldwork@gmail.com>
2023-05-03 20:44:33 -0500

    Add Directive for the hostlistener and hostbinding

--- src/app/_directives/phonemask.directive.ts
+++ src/app/_directives/phonemask.directive.ts
@@ -1,9 +1,32 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[digit-mask]'
 })
 export class PhonemaskDirective {
+
+  @HostBinding('style.border')
+  border:string
+
+  @HostListener('input', ['$event']) // you can listend to a click or on change as well
+  onKeyDown(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+
+    let trimmed = input.value.replace(/\s+/g, '');
+    if(trimmed.length > 16) {
+      trimmed = trimmed.substr(0, 16);
+    }
+    let numbers = [];
+    for(let i = 0; i < trimmed.length; i+=4) {
+      numbers.push(trimmed.substr(i,4));
+    }
+    input.value = numbers.join(' ');
+
+    this.border = '';
+    if(/[^\d]+/.test(trimmed)) {
+      this.border = '1px solid red';
+    }
+  }
 
   constructor(private element:ElementRef) {
       console.log(this.element);
