d936d5c3957c0ae8f5b5afdd07a427182d7d99ea
mike whitfield <whitfieldwork@gmail.com>
2023-05-29 12:28:36 -0500

    Add structure for stock selector

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
 import { StockProductsComponent } from './components/stock-products/stock-products.component';
+import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
 
 
 
 @NgModule({
-  declarations: [StockBranchComponent, StockProductsComponent],
+  declarations: [StockBranchComponent, StockProductsComponent, StockSelectorComponent],
   imports: [
     CommonModule
   ]
