f54a88a7fcba4f6d2f5a027c763a0590092ca721
mike whitfield <whitfieldwork@gmail.com>
2023-05-29 12:25:06 -0500

    Add structure for stock products

---
+++ src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-products',
+  templateUrl: './stock-products.component.html',
+  styleUrls: ['./stock-products.component.css']
+})
+export class StockProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
+import { StockProductsComponent } from './components/stock-products/stock-products.component';
 
 
 
 @NgModule({
-  declarations: [StockBranchComponent],
+  declarations: [StockBranchComponent, StockProductsComponent],
   imports: [
     CommonModule
   ]
